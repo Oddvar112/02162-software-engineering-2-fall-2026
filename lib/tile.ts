8ed5c4eb6cdd95238db9c5d33eca07eaac71e31a
Svendsen02 <sebastianthyregod@gmail.com>
2026-09-22 11:04:17 +0200

    Make different tile types

--- lib/tile.ts
+++ lib/tile.ts
@@ -14,5 +14,27 @@ interface LaserTile {
   strength: number;
 }
 
+interface PitTile {
+    kind: "pit"
+}
 
-type Tile = FloorTile | ConveyorTile | LaserTile;
+interface GearTile {
+    kind: "gear",
+    clockwise: boolean //Ture = Clockwise, False = Counter clockwise 
+}
+
+interface PusherTile {
+    kind: "pusher",
+    direction: Direction
+}
+
+interface CheckpointTile {
+    kind: "checkpoint",
+    number: number
+}
+
+interface RepairTile {
+    kind: "repair"
+}
+
+type Tile = FloorTile | ConveyorTile | LaserTile | PitTile | GearTile | PusherTile | CheckpointTile | RepairTile ;
