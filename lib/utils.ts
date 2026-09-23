8885932e32b624021d6a498fd6c194d9790ca584
Valdemar <valdemarfuglsang@gmail.com>
2026-09-23 21:23:23 +0200

    fix: retain hasEnvVars in lib/utils for middleware

--- lib/utils.ts
+++ lib/utils.ts
@@ -5,3 +5,8 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
+export const hasEnvVars = Boolean(
+  process.env.NEXT_PUBLIC_SUPABASE_URL &&
+    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY,
+);
+
